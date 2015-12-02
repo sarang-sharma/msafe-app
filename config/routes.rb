50135daae7ef36e0a4263090cdd422afcb58001e
sarang-sharma <sarang.sharma14@gmail.com>
2015-12-03 04:16:20 +0530

    Added root route

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   match ':controller(/:action(/:id))',:via => [:get, :post]
   get "people/index"
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'people#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
